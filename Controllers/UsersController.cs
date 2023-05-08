a80c749f868d79b2795c9b6e893f7ead5083f070
Erzal Fahrezy Primanda <fahrezyprimanda@gmail.com>
2023-05-08 12:48:36 +0700

    test2

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -12,5 +12,9 @@ namespace Goole_OpenId.Controllers
         {
             return View();
         }
+        public IActionResult test2()
+        {
+            return View();
+        }
     }
 }
