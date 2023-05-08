1fe0730b1a6fb68ca32d8b7730de5aeefc7492c7
fadli azlinaldi <adiaandio50@gmail.com>
2023-05-08 12:41:06 +0700

    "test commit"

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Goole_OpenId.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult test()
+        {
+           return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Goole_OpenId.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// services db
+string db = builder.Configuration.GetConnectionString("MyDb");
+builder.Services.AddDbContext<GooleDbContext>(options => options.UseSqlServer(db));
+
+// jwt
+var secret = builder.Configuration["AppSettings:Secret"];
+var secretBytes = Encoding.ASCII.GetBytes(secret);
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(o =>
+{
+    o.RequireHttpsMetadata = false;
+    o.SaveToken = true;
+    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(secretBytes)
+    };
+});
+
+// policy
+var policy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder(
+        JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build();
+builder.Services.AddAuthorization(o => o.DefaultPolicy = policy);
+
 
 var app = builder.Build();
 
