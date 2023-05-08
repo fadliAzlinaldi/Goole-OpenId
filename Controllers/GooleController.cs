7b42cf4023ee5e45798fba018e9354bf2ee5c46b
Fajar Embutara <fadjarembutara@gmail.com>
2023-05-08 15:08:49 +0700

    menambahkan controller register

--- Controllers/GooleController.cs
+++ Controllers/GooleController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Goole_OpenId.Dtos;
+using Goole_OpenId.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BC = BCrypt.Net.BCrypt;
 
 namespace Goole_OpenId.Controllers
 {
@@ -7,6 +10,56 @@ namespace Goole_OpenId.Controllers
     [ApiController]
     public class GooleController : ControllerBase
     {
+        private readonly GooleDbContext _context;
+        private readonly IConfiguration _configuration;
+        public GooleController(GooleDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public string Register(RegisterDto user)
+        {
+            // transaction
+            using (var trans = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    // tambah user
+                    var u = new User
+                    {
+                        Username = user.Username,
+                        Password = BC.HashPassword(user.Password),
+                        Fullname = user.Fullname,
+                        PhoneNumber = user.PhoneNumber,
+                        Email = user.Email,
+                        Address = user.Address,
+                        City = user.City
+                    };
 
+                    // ambil role member
+                    var role = _context.Roles.Where(o => o.NameRole == "member").FirstOrDefault();
+                    // assign role ke user
+                    if (role != null)
+                    {
+                        var ur = new UserRole();
+                        ur.User = u;
+                        ur.Role = role;
+
+                        _context.UserRoles.Add(ur);
+                        // simpan dan commit
+                        _context.SaveChanges();
+                        trans.Commit(); // commit
+                        return "sukses";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    trans.Rollback();
+                }
+            }
+            return "gagal";
+        }
     }
 }
