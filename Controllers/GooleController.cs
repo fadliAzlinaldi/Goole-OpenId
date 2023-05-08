6f926b64c6bebed436c0d4a3eede764202f2c46b
Wiwit Annisa Puspaningsari <129486382+wiwitannisa14@users.noreply.github.com>
2023-05-08 13:02:16 +0700

    Create GooleController

---
+++ Controllers/GooleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Goole_OpenId.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GooleController : ControllerBase
+    {
+    }
+}
